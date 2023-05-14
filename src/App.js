7171f50e33445f55b9a40cbe713b9629e71d62ea
unknown <swetheendra.t@gmail.com>
2023-05-14 16:33:26 -0600

    msla lib

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import Profile from './Profile';
 import { Route, Routes } from 'react-router-dom';
 
+import { useEffect } from "react"
+import { useMsal, useAccount, useIsAuthenticated } from "@azure/msal-react";
+
 function App() {
+
+  const isAuthenticated = useIsAuthenticated();
+  console.log('is authn....', isAuthenticated);
+  const { instance, accounts } = useMsal();
+  console.log('details....', instance, accounts);
+  const account = useAccount(accounts[0] || {});
+  console.log('details....', account);
+
+  useEffect(() => {
+    console.log('inside....');
+    if (account) {
+        instance.acquireTokenSilent({
+            scopes: ["User.Read"],
+            account: account
+        }).then((response) => {
+            console.log('token....',response.accessToken);
+        });
+    }
+}, [account, instance]);
+
   return (
     <div className="App">
       <Routes>
